3d412160d23ededb837b4c4fd30e65ddd5a36ca8
XuanInsr <2087868649@qq.com>
2023-01-24 22:36:40 +0800

    修改了pattern，增加了对公式前后空格的去除

--- replace_formula.py
+++ replace_formula.py
@@ -9,9 +9,10 @@ import re
 
 def replace_formula(file_name : str):
     with open(file_name, 'r', encoding='utf-8') as f:
-        pattern = r'!\[\]\(https://cdn\.nlark\.com/yuque/__latex/[^&]*&code=([^&]*)&[^)]*\)'
+        pattern = r'!\[\]\(https://[^&]*yuque[^&]*&code=([^&]*)&[^)]*\)'
         content = f.read()
         new = re.sub(pattern, replace, content)
+        # 如果想在源文件中修改，将下面的 `'replaced_' + ` 删除即可；但是记得备份！
         with open('replaced_' + file_name, 'w', encoding='utf-8') as fout:
             fout.write(new)
 
@@ -20,6 +21,6 @@ import urllib.parse
 def replace(matched):
     formula = matched.group(1)
     formula = urllib.parse.unquote(formula)
-    return '$' + formula + '$'
+    return '$' + formula.strip() + '$'
     
 replace_all_mds()
